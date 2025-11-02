2ecee3e1082856d4b5e7bd36104afc658c326c50
Aashika <khatiwadaaashika12@gmail.com>
2025-11-02 19:29:48 +0545

    cis

--- change-password.php
+++ change-password.php
@@ -15,11 +15,11 @@ if(!isset($_SESSION['login']) || $_SESSION['login'] == ''){
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
 <style>
 body { font-family:"Segoe UI", sans-serif; background:#f4f7fb; }
-.container { max-width:800px; margin:30px auto; padding:20px; background:rgba(200, 218, 233, 1); border-radius:12px; box-shadow:0 10px 15px rgba(0,0,0,0.1); }
+.container { max-width:600px; margin:90px auto; padding:20px; background:rgba(200, 218, 233, 1); border-radius:12px; box-shadow:0 10px 15px rgba(0,0,0,0.1); }
 h2 { text-align:center; color:#2A5D91; margin-bottom:20px; }
 .change-pw { display:grid; grid-template-columns:1fr 2fr; gap:10px; margin-top:20px; }
 .change-pw { padding:5px 0; }
-.label { font-weight:bold; color:black; }
+.label { font-weight:bold; color:black; font-size:17px; }
 .changeBtn { display:block; margin:20px auto; padding:10px 20px; background:#2A5D91; color:white; border:none; border-radius:8px; cursor:pointer; text-decoration:none; text-align:center; width:200px; }
 .changeBtn:hover { background:#1F456F; }
 </style>
@@ -28,14 +28,17 @@ h2 { text-align:center; color:#2A5D91; margin-bottom:20px; }
 
 <div class="container">
     <h2> <?php echo $_SESSION['login']; ?> </h2>
-   
+   <form method="post">
     <div class="change-pw">
-        <div class="label" name="oldPw">Enter old password</div> 
-        <div class="label" name="newPw">Enter new password</div> 
-        <div class="label" name="confirmPw">Confirm new password</div> 
+        <div class="label">Enter old password:</div> 
+        <input type="password" name="oldPw">
+        <div class="label">Enter new password:</div> 
+        <input type="password" name="newPw" >
+        <div class="label" >Confirm new password:</div>
+        <input type="password" name="confirmPw"> 
         <button class="changeBtn" type="submit">Change Password</button>
          </div>
-
+<form>
 </div>
 
 </body>
