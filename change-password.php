5bc080f9a6270d0ee3df965f120305aa907eece9
Aashika <khatiwadaaashika12@gmail.com>
2025-11-04 18:59:13 +0545

    cis

--- change-password.php
+++ change-password.php
@@ -6,37 +6,28 @@ if(!isset($_SESSION['login']) || $_SESSION['login'] == ''){
     exit;
 }
 $message="";
+$updateMessage="";
 if(isset($_POST['changeBtn'])){
     $oldPw= md5($_POST['oldPw']);
     $newPw=md5($_POST['newPw']);
     $confirmPw=md5($_POST['confirmPw']);
     $username=$_SESSION['login'];
-
-     if($oldPw==="" || $newPw==="" || $confirmPw===""){
-        $message .="Please fill all the fields.";
-     }
-else{
+   
     $sql="Select password from users where password=? AND username=?";
     $stmt=$pdo->prepare($sql);
     $stmt->execute([$oldPw,$username]);
-    $result=$stmt->fetchAll(PDO::FETCH_ASSOC);
+    $result=$stmt->fetch(PDO::FETCH_ASSOC);
    
-    if($result && ($newPw== $confirmPw)){
+    if($result && ($result['password']==$oldPw)){
         $sql="UPDATE users SET password=? where username=?";
         $stmt=$pdo->prepare($sql);
         $updatepw=$stmt->execute([$newPw,$username]);
-
         if($updatepw){
-            $message .= " Password changed successfully.";
-        }
-        else{
-            $message .= " New passwords do not match with confirmation password.";
-        }
-    }
+            $updateMessage .= " Password changed successfully.";
+        }}
  else{
-        $message .= " Old password do not match.";
-    }
-}}
+        $message .= "Old password does not match.";
+   } }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -60,19 +51,70 @@ h2 { text-align:center; color:#2A5D91; margin-bottom:20px; }
 <div class="container">
     
 <p style="text-align:center; color:red; margin:10px;"><?php echo $message; ?></p>
+
+<p style="text-align:center; color:green; margin:10px;"><?php echo $updateMessage; ?></p>
     <h2> <?php echo $_SESSION['login']; ?> </h2>
-   <form method="post">
+
+   <form method="post" onSubmit="return validateForm()">
     <div class="change-pw">
         <div class="label">Enter old password:</div> 
-        <input type="password" name="oldPw">
+        <input type="password" name="oldPw" id="oldpw">
+         <p id="oldPw-err" style="color:red;"></p>
+      
         <div class="label">Enter new password:</div> 
-        <input type="password" name="newPw" >
+        <input type="password" name="newPw" id="newpw"
+        title="Must contain at least 1 number,an uppercase and a lowercase letter, and at least 6 or more characters">
+         <p id="newPw-err" style="color:red;"></p>
+
         <div class="label" >Confirm new password:</div>
-        <input type="password" name="confirmPw"> 
+        <input type="password" name="confirmPw" id="confirmpw"> 
+         <p id="confirmPw-err" style="color:red;"></p>
+      
         <button class="changeBtn" name="changeBtn" type="submit">Change Password</button>
          </div>
 </form>
 </div>
 
+<script>
+  function validateForm(){
+  var oldPw= document.getElementById("oldpw").value.trim();
+  var newPw= document.getElementById("newpw").value.trim();
+   var confirmPw= document.getElementById("confirmpw").value.trim();
+
+  document.getElementById("oldPw-err").innerHTML = "";
+  document.getElementById("newPw-err").innerHTML = "";
+  document.getElementById("confirmPw-err").innerHTML = "";
+
+  let valid=true;
+  // checking for empty un field
+
+  if(oldPw===""){
+    document.getElementById("oldPw-err").innerHTML="Please enter the old password";
+    valid=false;
+  }
+
+  // password validation
+  const pwPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
+
+  if(newPw===""){
+    document.getElementById("newPw-err").innerHTML="Please enter new password";
+    valid=false;
+  }
+  else if (!pwPattern.test(newPw)) {
+    document.getElementById("newPw-err").innerHTML="Password must contain at least one number, one uppercase, one lowercase letter, and be 6 or more characters long.";
+    valid=false;
+  }
+// for pw confirmation
+ if(confirmPw===""){
+    document.getElementById("confirmPw-err").innerHTML="Please enter this field for password confirmation";
+    valid=false;
+  }
+  else if (newPw !== confirmPw) {
+   document.getElementById("confirmPw-err").innerHTML="New password do not match with confirmation password!";
+    valid= false;
+  }
+      return valid;
+    }
+  </script>
 </body>
 </html>
