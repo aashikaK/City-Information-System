19ecac35ba13b252e2656b4f6ae95d12df08b969
Aashika <khatiwadaaashika12@gmail.com>
2025-11-04 19:24:07 +0545

    cis

--- change-password.php
+++ change-password.php
@@ -39,7 +39,6 @@ if(isset($_POST['changeBtn'])){
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Change Password - City Information System</title>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
-
 <style>
 body { font-family:"Segoe UI", sans-serif; background:#f4f7fb; }
 .container { max-width:600px; margin:90px auto; padding:20px; background:rgba(200, 218, 233, 1); border-radius:12px; box-shadow:0 10px 15px rgba(0,0,0,0.1); }
@@ -47,38 +46,8 @@ h2 { text-align:center; color:#2A5D91; margin-bottom:20px; }
 .change-pw { display:grid; grid-template-columns:1fr 2fr; gap:10px; margin-top:20px; }
 .change-pw div{ padding:5px 0; }
 .label { font-weight:bold; color:black; font-size:17px; }
-
-input[type="password"] {
-  width: 95%;
-  padding: 10px 12px;
-  border: 1.5px solid #ccc;
-  border-radius: 8px;
-  font-size: 15px;
-  transition: all 0.3s ease;
-  outline: none;
-}
-
-input[type="password"]:focus {
-  border-color: #2A5D91;
-  box-shadow: 0 0 5px rgba(42, 93, 145, 0.4);
-}
-
-.changeBtn { 
-  display:block; 
-  margin:20px auto; 
-  padding:10px 20px; 
-  background:#2A5D91; 
-  color:white; 
-  border:none; 
-  border-radius:8px; 
-  cursor:pointer; 
-  text-decoration:none; 
-  text-align:center; 
-  width:200px; 
-}
+.changeBtn { display:block; margin:20px auto; padding:10px 20px; background:#2A5D91; color:white; border:none; border-radius:8px; cursor:pointer; text-decoration:none; text-align:center; width:200px; }
 .changeBtn:hover { background:#1F456F; }
-
-
 </style>
 </head>
 <body>
